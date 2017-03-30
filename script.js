f0f585098de381646374634a94c1ddb4878caa2a
GoForMarty <marty.stepien@gmail.com>
2017-03-30 22:04:15 +0200

    blanks from answer function added

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 var words = ['tiger', 'horse', 'wolf', 'squirrel', 'giraffe'];
+var randomWord =  words[Math.floor(Math.random()*(words.length - 1))];
 
-// Selecting random word to guess
-function chooseWord () {
-    return words[Math.floor(Math.random()*(words.length - 1))];
+var str = '';
+var count = 0;
+var answerArray = [];
+
+
+function blanksFromAnswer() {
+    
+    for (var i=0; i<randomWord.length; i++) {
+      answerArray[i]='_';
+    }
+    str = answerArray.join(' ');
+    return str + " " + answerArray;
 }
 
-chooseWord();
+blanksFromAnswer(randomWord);
